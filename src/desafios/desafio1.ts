ef663b1789314f20517d5d065387f137f465809f
hellensrodex <hellensrodex@hotmail.com>
2022-07-12 07:52:26 -0300

    desafios typescript

---
+++ src/desafios/desafio1.ts
@@ -0,0 +1,8 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee3: { code: number, name: string } ={
+    code: 4,
+    name: "John",
+}
+
+console.log(`Código ${employee3.code} e o nome ${employee3.name} \n`);
